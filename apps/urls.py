d4a3dd99d1d774e700e792d7a61ee44bbf1ded42
tuenut <unnetu@gmail.com>
2020-06-20 12:39:41 +0300

    --

--- apps/urls.py
+++ apps/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
     favicon_path,
     path('', index, name="index"),  # spa
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls), name='api'),
+    path('api/', include(router.urls)),
     url(r'api/auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
