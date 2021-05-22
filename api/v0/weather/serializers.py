d8cb305d08bd48e4785f0ac9211b6de1be21a1e1
groshev artem <groshev41273m@gmail.com>
2021-05-22 13:05:56 +0300

    fix

--- api/v0/weather/serializers.py
+++ api/v0/weather/serializers.py
@@ -8,14 +8,15 @@ __all__ = ['OWMWeatherSerializer', 'OWMDataSerializer']
 class OWMWeatherSerializer(serializers.ModelSerializer):
     class Meta:
         model = OWMWeather
-        fields = ['owm_id', 'name', 'description', 'icon']
+        fields = ["id", 'owm_id', 'name', 'description', 'icon']
 
 
-class OWMDataSerializer(serializers.HyperlinkedModelSerializer):
-    # TODO temporally while spa in development. Must be remove in future,
-    #  because spa must make api_request for get image.
+class OWMDataSerializer(serializers.ModelSerializer):
     weather_data = OWMWeatherSerializer(source='weather')
 
     class Meta:
         model = OWMData
-        fields = ['timestamp', 'temperature', 'pressure', 'humidity', 'weather', 'weather_data']
+        fields = [
+            "id", 'timestamp', 'temperature', 'pressure', 'humidity',
+            'weather', 'weather_data'
+        ]
